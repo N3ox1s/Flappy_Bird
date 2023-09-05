a82a29e2d3f97ec8987db1499a1fe792c2d3adbf
TH3xpert <trapp.mz@gmail.com>
2023-09-05 13:27:33 +0200

    laufende pipes

--- interface.py
+++ interface.py
@@ -1,6 +1,6 @@
 import pygame
 import math
-
+import random
 
 class Interface(object):
     def __init__(self):
@@ -10,6 +10,8 @@ class Interface(object):
         self.clock = pygame.time.Clock()
         self.keyboard_interrupt = False
         self.jumped = False
+        self.movable_pipe_distance = 0
+        self.pipes = []
 
     def interface_loop(self, physics):
         for event in pygame.event.get():
@@ -21,23 +23,30 @@ class Interface(object):
         for i in range(self.tiles_bg):
             self.screen.blit(self.bg_day, (i * self.bg_width + self.bg_scroll, 0))
 
-        self.screen.blit(self.pipe_green, (self.width + self.pipe_scroll, 100))
+        self.movable_pipe_distance -= 2
+
+        if self.movable_pipe_distance <= 0:
+            self.pipes.append(pipe())
+            self.movable_pipe_distance = physics.pipe_distance
+            self.pipes[-1].set_pipe_length(random.randint(150, self.height-100))
+
+        for i in self.pipes:
+            if abs(i.pipe_scroll) >= self.width + self.pipe_green.get_width() + 300:
+                self.pipes.remove(i)
+            i.move()
+            self.screen.blit(self.pipe_green, (self.width + i.pipe_scroll, i.lower_pipe_length))
 
         for i in range(self.tiles_base):
             self.screen.blit(self.bg_base, (i * self.base_width + self.fg_scroll, 400))
 
         self.bg_scroll -= 1
         self.fg_scroll -= 2
-        self.pipe_scroll -= 2
 
         if abs(self.bg_scroll) > self.bg_width:
             self.bg_scroll = 0
 
         if abs(self.fg_scroll) > self.bg_width:
             self.fg_scroll = 0
-
-        if abs(self.pipe_scroll) >= self.width + self.pipe_green.get_width():
-            self.pipe_scroll = 0
 
         self.screen.blit(self.current_bird_sprite(), (self.width // 2, physics.pos_y))
 
@@ -65,9 +74,21 @@ class Interface(object):
         self.base_width = self.bg_base.get_width()
         self.bg_scroll = 0
         self.fg_scroll = 0
-        self.pipe_scroll = 0
         self.tiles_bg = math.ceil(self.width / self.bg_width) + 1
         self.tiles_base = math.ceil(self.width / self.base_width) + 1
 
         pygame.display.set_icon(self.icon)
         pygame.display.set_caption("Flappy Bird")
+
+
+class pipe(object):
+    def __init__(self):
+        self.pipe_scroll = 0
+        self.lower_pipe_length = 0
+
+    def move(self):
+        self.pipe_scroll -= 2
+
+    def set_pipe_length(self, lower_pipe_length):
+        self.lower_pipe_length = lower_pipe_length
+
--- physics.py
+++ physics.py
@@ -3,7 +3,7 @@ class Physics(object):
         self.pos_y = 100
         self.gravity = 500
         self.velocity = 0
-        self.tube_distance = 100
+        self.pipe_distance = 300
         self.score = 0
         self.counter = 0
         self.dt = 0
@@ -19,7 +19,7 @@ class Physics(object):
         self.velocity -= self.gravity * self.dt
         self.pos_y -= self.velocity * self.dt
         self.counter += 1
-        if self.counter == self.tube_distance:
+        if self.counter == self.pipe_distance:
             self.counter = 0
             self.score += 1
             print(self.score)
